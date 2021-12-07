0da6510654997b87ef8eba03efac90722a64bfa9
JRichtar <JRichtar@users.noreply.github.com>
2021-12-07 13:51:58 +0100

    .

--- Formulář/MainWindow.xaml.cs
+++ Formulář/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,221 +13,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.ComponentModel;
+
 
 namespace Formulář
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        Employee em;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            em = new Employee { FirstName = "Missing", LastName = "Missing", YearOfBirth = "Missing", Job = "Missing", Payment = "Missing" };
-
-            this.DataContext = em;
-            labelErrorFn.DataContext = this;
-            labelErrorLn.DataContext = this;
-            labelErrorBd.DataContext = this;
-            labelErrorJob.DataContext = this;
-            labelErrorPm.DataContext = this;
+            University.InitUniversities();
+            Universities.DataContext = University.AllUniversities;
         }
 
-
-
-        private bool CheckFirstName()
+        private void Universities_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (em.FirstName.Length > 0)
-            {
-                FnErrorVisible = Visibility.Hidden;
-                return true;
-            }
-            else
-            {
-                FnErrorVisible = Visibility.Visible;
-                return false;
-            }
+            University u = ((KeyValuePair<string, University>)(sender as ListView).SelectedItem).Value;
+            DataContext = u;
         }
 
-        private Visibility _FnErrorVisible;
-        public Visibility FnErrorVisible
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            get { return _FnErrorVisible; }
-            set
-            {
-                _FnErrorVisible = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("FnErrorVisible"));
-                }
-            }
-        }
-
-        private string _FnError = "Needs to be filled!";
-        public string FnError { get { return _FnError; } }
-
-        private bool CheckLastName()
-        {
-            if (em.LastName.Length > 0)
-            {
-                LnErrorVisible = Visibility.Hidden;
-                return true;
-            }
-            else
-            {
-                LnErrorVisible = Visibility.Visible;
-                return false;
-            }
-        }
-
-        private Visibility _LnErrorVisible;
-        public Visibility LnErrorVisible
-        {
-            get { return _LnErrorVisible; }
-            set
-            {
-                _LnErrorVisible = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("LnErrorVisible"));
-                }
-            }
-        }
-
-        private string _LnError = "Needs to be filled!";
-        public string LnError { get { return _LnError; } }
-
-        private bool CheckYear()
-        {
-            if (em.YearOfBirth.Length > 0)
-            {
-                BdErrorVisible = Visibility.Hidden;
-                return true;
-            }
-            else
-            {
-                BdErrorVisible = Visibility.Visible;
-                return false;
-            }
-        }
-
-        private Visibility _BdErrorVisible;
-        public Visibility BdErrorVisible
-        {
-            get { return _BdErrorVisible; }
-            set
-            {
-                _BdErrorVisible = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("BdErrorVisible"));
-                }
-            }
-        }
-
-        private string _BdError = "Needs to be filled";
-        public string BdError { get { return _BdError; } }
-
-        private bool CheckJob()
-        {
-            if (em.Job.Length > 0)
-            {
-                JobErrorVisible = Visibility.Hidden;
-                return true;
-            }
-            else
-            {
-                JobErrorVisible = Visibility.Visible;
-                return false;
-            }
-        }
-
-        private Visibility _JobErrorVisible;
-        public Visibility JobErrorVisible
-        {
-            get { return _JobErrorVisible; }
-            set
-            {
-                _JobErrorVisible = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("JobErrorVisible"));
-                }
-            }
-        }
-
-        private string _JobError = "Needs to be filled!";
-        public string JobError { get { return _JobError; } }
-
-        private bool CheckPayment()
-        {
-            if (em.Payment.Length > 0)
-            {
-                PmErrorVisible = Visibility.Hidden;
-                return true;
-            }
-            else
-            {
-                PmErrorVisible = Visibility.Visible;
-                return false;
-            }
-        }
-
-        private Visibility _PmErrorVisible;
-        public Visibility PmErrorVisible
-        {
-            get { return _PmErrorVisible; }
-            set
-            {
-                _PmErrorVisible = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("PmErrorVisible"));
-                }
-            }
-        }
-
-        private string _PmError = "Needs to be filled";
-        public string PmError { get { return _PmError; } }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show($"First name: {em.FirstName} \nLast name: {em.LastName} \nYear of birth: {em.YearOfBirth} \nEducation: {em.HighestEducation} \nJob: {em.Job} \nPayment: {em.Payment}");
-            em = new Employee { FirstName = "Missing", LastName = "Missing", YearOfBirth = "Missing", Job = "Missing", Payment = "Missing"};
-            Employee.AllEmployees.Add(em);
-            this.DataContext = em;
-        }
-
-        private void textboxFn_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            CheckFirstName();
-        }
-
-        private void textboxLn_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            CheckLastName();
-        }
-
-        private void textboxBd_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            CheckYear();
-        }
-
-        private void textboxJob_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            CheckJob();
-        }
-
-        private void textboxPm_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            CheckPayment();
+            Universities.SelectedIndex = 0;
         }
     }
 }
---
+++ Formulář/University.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Formulář
+{
+    class University : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        private string state;
+        public string State
+        {
+            get => state;
+            set { state = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("State")); }
+        }
+
+
+
+        private int established;
+        public int Established
+        {
+            get => established;
+            set { established = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Established")); }
+        }
+
+
+
+        private string address;
+        public string Address
+        {
+            get => address;
+            set { address = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Address")); }
+        }
+
+
+
+        private double successRate;
+        public double SuccessRate
+        {
+            get => successRate;
+            set { successRate = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SuccessRate")); }
+        }
+        private decimal tuition;
+        public decimal Tuition
+        {
+            get => tuition;
+            set { tuition = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tuition")); }
+        }
+
+
+
+        public static Dictionary<string, University> AllUniversities = new Dictionary<string, University>();
+
+
+
+        public static void InitUniversities()
+        {
+            AllUniversities["MIT"] = new University
+            {
+                State = "Massachusets",
+                Established = 1861,
+                Address = "Cambridge, USA",
+                SuccessRate = 93.0,
+                Tuition = 19700.0m
+            };
+
+
+
+            AllUniversities["Yale"] = new University
+            {
+                State = "New Haven",
+                Established = 1701,
+                Address = "New Haven, USA",
+                SuccessRate = 94.0,
+                Tuition = 18700.0m
+            };
+
+
+
+            AllUniversities["Cornell"] = new University
+            {
+                State = "New York",
+                Established = 1865,
+                Address = "Ithaca, NY, USA",
+                SuccessRate = 94.0,
+                Tuition = 69700.0m
+            };
+        }
+    }
+}
